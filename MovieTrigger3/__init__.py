554dd4f4afafd4a602b30cf035b5a030f9d544f7
Siddhartha Srivastava <sid7513@hotmail.com>
2020-10-18 22:51:04 +0530

    added files

---
+++ MovieTrigger3/__init__.py
@@ -0,0 +1,20 @@
+import json
+import logging
+import pandas as pd
+import azure.functions as func
+
+
+def main(event: func.EventGridEvent):
+    result = json.dumps({
+        'id': event.id,
+        'data': event.get_json(),
+        'topic': event.topic,
+        'subject': event.subject,
+        'event_type': event.event_type,
+    })
+    
+    logging.info('Python EventGrid trigger processed an event: %s', result)
+    d = {'a':[1,2], 'b':[3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+
